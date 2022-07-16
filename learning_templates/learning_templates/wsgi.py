509124809b827f3933ac805089e04492ada7afa4
mladen21bl <mladenpopovic50@.com>
2022-07-16 20:36:38 +0200

    first commit

---
+++ learning_templates/learning_templates/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for learning_templates project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_templates.settings")
+
+application = get_wsgi_application()
